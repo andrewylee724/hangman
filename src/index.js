0b222f326948c95c7b1c4a8e32fab61dc532c98c
Andrew Lee <yenju.andrew.lee@gmail.com>
2018-12-06 23:54:34 -0500

    REACT: refactored application using React

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hangman from './Hangman';
+import './index.css';
+
+ReactDOM.render(<Hangman />, document.getElementById('root'));
