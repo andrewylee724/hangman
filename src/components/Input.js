ffdb9f4b0ec0882efd5230b296ed11e33d593331
Andrew Lee <yenju.andrew.lee@gmail.com>
2018-12-09 02:37:53 -0500

    feature-2: added visual keyboard

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/Input.scss';
 
 class Input extends React.Component {
 
@@ -23,15 +24,49 @@ class Input extends React.Component {
     // }, this.checkGuess);
   }
 
-  render() {
-    const { submitGuess } = this.props;
+  handleKeyClick = () => {
+    console.log('key is clicked!');
+  }
 
+  renderKeyboard = () => {
+
+    const convertToKeys = (str) => {
+      return str.split('').map(key => {
+        return (
+          <span 
+            key={key}
+            className="key"
+            onClick={this.handleKeyClick}>
+            {key}
+          </span>
+        );
+      })
+    };
+
+    return (
+      <div className="keyboard">
+        <div>
+          {convertToKeys('qwertyuiop')}
+        </div>
+        <div>
+          {convertToKeys('asdfghjkl')}
+        </div>
+        <div>
+          {convertToKeys('zxcvbnm')}
+        </div>
+      </div>
+    );
+  }
+
+  render() {
     return (
       <section>
         <form onSubmit={this.submitGuess}>
           <input className="interactions" ref={node => { this.input = node }} type="text" />
           <input className="interactions" type="submit" value="Submit" />
         </form>
+
+        {this.renderKeyboard()}
       </section>
     );
   }
