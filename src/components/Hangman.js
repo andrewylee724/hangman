e0a73607842e68c47eef0bf985193a81edd1af9d
Andrew Lee <yenju.andrew.lee@gmail.com>
2018-12-11 07:41:09 -0500

    feature-6: refactored several files so that the incorrect guesses are displayed in a sidebar

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Display from '../containers/Display';
 import Input from '../containers/Input';
+import IncorrectGuesses from '../containers/IncorrectGuesses';
 
 const HangMan = () => {
   return (
-    <div className="wrapper">
-      <Display />
-      <Input />
+    <div className="game_wrapper">
+      <main className="wrapper main">
+        <Display />
+        <Input />
+      </main>
+      <aside className="wrapper side">
+        <IncorrectGuesses />
+      </aside>
     </div>
   );
 }
