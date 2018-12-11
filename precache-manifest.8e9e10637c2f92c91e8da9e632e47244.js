1bd933ffe6f49aaab8fad0bd71b34b904f357bc7
Andrew Lee <Andrew.Lee2@bjss.com>
2018-12-11 13:17:00 -0500

    Updates

---
+++ precache-manifest.8e9e10637c2f92c91e8da9e632e47244.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e81b4b6d0834211a4833",
+    "url": "/hangman/static/js/runtime~main.e81b4b6d.js"
+  },
+  {
+    "revision": "29a1e39b26491144137b",
+    "url": "/hangman/static/js/main.29a1e39b.chunk.js"
+  },
+  {
+    "revision": "62d4fdeca33edb165bc1",
+    "url": "/hangman/static/js/1.62d4fdec.chunk.js"
+  },
+  {
+    "revision": "29a1e39b26491144137b",
+    "url": "/hangman/static/css/main.6023a0d1.chunk.css"
+  },
+  {
+    "revision": "4737d47042057b9e16edd6d651fb73ff",
+    "url": "/hangman/index.html"
+  }
+];
